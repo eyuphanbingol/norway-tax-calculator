d8979312836055f14014a04dcaa87b6bec9286e1
eyuphanbingol <7smk2rs76x@privaterelay.appleid.com>
2025-12-06 00:56:13 +0300

    Robots.txt dosyasi eklendi - Google davetiyesi

---
+++ src/app/robots.js
@@ -0,0 +1,13 @@
+export default function robots() {
+  // Pazar günü domain alınca burayı güncelle: https://seninsiten.com
+  const baseUrl = 'https://norway-tax-calculator.vercel.app'; 
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/', // (Varsa gizli klasörler, şu an yok ama kalsın)
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
